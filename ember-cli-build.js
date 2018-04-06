08651eec8e430e6e528fa58aeacb7a0512c88276
Rui-Santos <rui.s@gmx.pt>
2018-04-06 23:15:51 +0100

    Update moment dependency, import datetimepicker locale

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -24,5 +24,8 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  // import locale for bootstrap-datepicker
+  app.import('node_modules/bootstrap-datepicker/js/locales/bootstrap-datepicker.pt.js');
+
   return app.toTree();
 };
